25ef41b8ea36a68ab323eb50b400a8fdf17eaa2e
Krishna Lagad <krishnalagad2@gmail.com>
2023-10-02 17:08:21 +0530

    - optimizing code.

--- src/main/java/com/learnvertx/starter/Application.java
+++ src/main/java/com/learnvertx/starter/Application.java
@@ -5,7 +5,9 @@ import io.vertx.core.Vertx;
 public class Application {
 
   public static void main(String[] args) {
-    Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(new MainVerticle());
+//    Vertx vertx = Vertx.vertx();
+//    vertx.deployVerticle(new MainVerticle());
+
+    Vertx.vertx().deployVerticle(new MainVerticle());
   }
 }
