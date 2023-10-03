b966e1401e64bda4570bdd1018ba92be058a5d16
Krishna Lagad <krishnalagad2@gmail.com>
2023-10-03 17:09:12 +0530

    - dump method to create  task

--- src/main/java/com/learnvertx/starter/repository/TaskRepositoryImpl.java
+++ src/main/java/com/learnvertx/starter/repository/TaskRepositoryImpl.java
@@ -1,17 +1,24 @@
 package com.learnvertx.starter.repository;
 
+import com.learnvertx.starter.model.Task;
 import com.learnvertx.starter.model.TaskDto;
 import com.learnvertx.starter.model.TasksList;
 import io.vertx.core.Future;
 import org.hibernate.reactive.stage.Stage;
 
 import java.util.Optional;
+import java.util.concurrent.CompletionStage;
 
 public record TaskRepositoryImpl(Stage.SessionFactory sessionFactory) implements TaskRepository {
 
   @Override
   public Future<TaskDto> createTask(TaskDto task) {
-    return null;
+    TaskEntityMapper entityMapper = new TaskEntityMapper();
+    Task entity = entityMapper.apply(task);
+    CompletionStage<Void> result = sessionFactory.withTransaction((s, t) -> s.persist(entity));
+    TaskDtoMapper dtoMapper = new TaskDtoMapper();
+    Future<TaskDto> future = Future.fromCompletionStage(result).map(v -> dtoMapper.apply(entity));
+    return future;
   }
 
   @Override
